86b041a0fefaa5f25c38609730efbe4343994dc1
antaripa04 <antaripapatra99@gmail.com>
2021-07-23 00:13:04 +0530

    Backend completed

---
+++ BACKEND/src/models/user.model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+    email: { type: String, require: true },
+    }
+);
+
+module.exports = mongoose.model("email", userSchema);
